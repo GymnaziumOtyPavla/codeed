6f0104723e80dcf32b730f299cd100ee89743f84
thomasfurst99 <tomasfurst99@gmail.com>
2016-07-11 11:35:36 +0200

    Obdelnik

---
+++ java/begin/Obdelnik.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String... args) {
+    for (int i = 0; i < 20; i++) {
+      for (int j = 0; j < 50; j++) {
+        System.out.print("*");
+      }
+    }
+  }
+}
