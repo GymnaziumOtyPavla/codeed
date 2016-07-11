aa8184d24cd35fa155d3bb0e8a9be4d5957798bd
thomasfurst99 <tomasfurst99@gmail.com>
2016-07-11 13:13:14 +0200

    Updating and fixing

--- java/begin/Obdelnik.java
+++ java/begin/Obdelnik.java
@@ -1,10 +1,10 @@
-public class Main {
+public class Obdelnik {
   public static void main(String... args) {
     for (int i = 0; i < 20; i++) {
       for (int j = 0; j < 50; j++) {
         System.out.print("*");
       }
+      System.out.println();
     }
-    System.out.println();
   }
 }
--- java/begin/Trojuhelnik.java
+++ java/begin/Trojuhelnik.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Trojuhelnik {
   public static void main(String... args) {
     for (int i = 1; i <= 10; i++) {
       for (int j = 0; j < i; j++) {
