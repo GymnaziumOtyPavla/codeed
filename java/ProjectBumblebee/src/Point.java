d656af0a0c5300ba998a3c6058005f8e82a08081
Joulinkos <julie.barnova@gmail.com>
2016-07-14 13:21:38 +0200

    Point first version

--- java/ProjectBumblebee/src/Point.java
+++ java/ProjectBumblebee/src/Point.java
@@ -1,21 +1,26 @@
+
 /**
  *
- * @author thomasfurst
+ * @author joulinkos and peeta
  */
 public class Point {
+
+    private double x;
+    private double y;
+
     public double getX() {
         return 0;
     }
-    
+
     public void setX(double x) {
-        
+
     }
-    
+
     public double getY() {
         return 0;
     }
-    
+
     public void setY(double y) {
-        
+
     }
 }
