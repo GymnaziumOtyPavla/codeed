2c7f5e03966f5a40fa283fadd66dce03500fe291
Tomas pavlov <Tomas pavlov>
2016-07-14 14:08:59 +0200

    Vector first version

--- java/ProjectBumblebee/src/Vector.java
+++ java/ProjectBumblebee/src/Vector.java
@@ -1,23 +1,54 @@
 /**
  *
- * @author thomasfurst
+ * @author tomaspavlov
  */
 public class Vector {
-    
+
+    private Point point;
+
+    public Vector() {
+
+    }
+
+    public Vector(double x, double y) {
+
+    }
+
     public Vector add(Vector vector) {
-        return null;
+        Vector res = new Vector();
+        res.point.setX(point.getX() + vector.point.getX());
+
+        res.point.setY(point.getY() + vector.point.getY());
+
+        return res;
     }
-    
-    public Vector mult(int num) {
-        return null;
+
+    public Vector mult(double num) {
+
+        Vector res = new Vector();
+
+        res.point.setX(point.getX() * num);
+        res.point.setY(point.getY() * num);
+
+        return res;
+
     }
-    
-    public Vector normalize() {
-        return null;
-    }
-    
+
     public double getLen() {
-        return 0;
+
+        return Math.sqrt((point.getX() * point.getX()) / (point.getY() * point.getY()));
+
     }
-    
+
+    public Vector normalize() {
+
+        double len = getLen();
+
+        Vector res = new Vector();
+        res.point.setX(point.getX() / len);
+        res.point.setY(point.getY() / len);
+        return res;
+
+    }
+
 }
